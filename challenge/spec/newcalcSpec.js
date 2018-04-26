259b00de169403ca48c0769b69c610a413138608
Luisa Bolado <mboladop@hotmail.com>
2018-04-26 15:57:10 +0000

    drink about challenge testing

--- challenge/spec/newcalcSpec.js
+++ challenge/spec/newcalcSpec.js
@@ -65,7 +65,6 @@ describe("Extra calculator functions", function() {
         })
     });
     
- 
     describe("Area of a Cylinder function", function(){
        
         it("should return an area of 100.53 for a radius of 2 and a height of 6", function(){
@@ -73,5 +72,65 @@ describe("Extra calculator functions", function() {
         });
     });
 
-     
+    describe("FizzBuzz game function", function(){
+        it("should return fizzbuzz if the number is divisible by 3 or 5", function(){
+            expect(fizzBuzz(15)).toBe("Fizz Buzz");
+        });
+        
+        it("should return fizz if the number is divisible by 3", function(){
+            expect(fizzBuzz(9)).toBe("Fizz");
+        });
+        
+        it("should return fizzbuzz if the number is divisible by 5", function(){
+            expect(fizzBuzz(10)).toBe("Buzz");
+        });
+        
+        it("should return fizzbuzz if the number is not divisible by 3 or/ and 5", function(){
+            expect(fizzBuzz(8)).toBe(8);
+        });
+        
+        it("should alert NaN if the argument is not a number", function(){
+            spyOn(window, "alert");
+            fizzBuzz("goat");
+            expect(window.alert).toHaveBeenCalledWith("Insert a number Dummy!");
+        });
+    });
+    
+    describe("What can i drink function", function() {
+       
+       it("should alert sorry i cant tell what drink because that age is incorrect, introduce a number!", function() {
+           spyOn(window, "alert");
+           whatCanIDrink("string");
+           expect(window.alert).toHaveBeenCalledWith("Sorry, i cant tell what drink because that age is incorrect, introduce a number!");
+
+       });
+
+       it("should alert sorry, but either you haven´t been born or you are already dead, enter a number between 0 and 130!", function() {
+           spyOn(window, "alert");
+           whatCanIDrink("-3");
+           expect(window.alert).toHaveBeenCalledWith("Sorry, but either you haven´t been born or you are already dead, enter a number between 0 and 130!");
+       });
+
+       it("should alert Drink Toddy!", function() {
+           spyOn(window, "alert");
+           whatCanIDrink("12");
+           expect(window.alert).toHaveBeenCalledWith("Drink Toddy!");
+       });
+       it("should alert Drink Coke!", function() {
+           spyOn(window, "alert");
+           whatCanIDrink("15");
+           expect(window.alert).toHaveBeenCalledWith("Drink Coke!");
+       });
+       it("should alert Drink Beer!", function() {
+           spyOn(window, "alert");
+           whatCanIDrink("19");
+           expect(window.alert).toHaveBeenCalledWith("Drink Beer!");
+       });
+       it("should alert Drink Whisky!", function() {
+           spyOn(window, "alert");
+           whatCanIDrink("105");
+           expect(window.alert).toHaveBeenCalledWith("Drink Whisky!");
+       });
+       
+       });
 });
--- challenge/src/calcFunctions.js
+++ challenge/src/calcFunctions.js
@@ -45,9 +45,30 @@ function areaOfCylinder(r, h) {
     return ( ((2 * r)*h ) + (2 * (r * r)) ) * Math.PI ;
 }
 
-/*
 function fizzBuzz(n) {
-    if (n % 3 == 0) {
-    return "fizz";
-  }
-}*/
+    if (isNaN(n)) {
+      alert ("Insert a number Dummy!");
+  } else if (n % 3 == 0 && n % 5 == 0) {
+    return "Fizz Buzz";
+  } else if (n % 3 == 0){
+    return "Fizz";
+  } else if (n % 5 == 0){
+    return "Buzz";
+  } else {return n;}
+}
+
+function whatCanIDrink(n){
+    if (isNaN(n)) {
+        alert("Sorry, i cant tell what drink because that age is incorrect, introduce a number!");
+    } else if (n < 0 || n >= 130) {
+        alert("Sorry, but either you haven´t been born or you are already dead, enter a number between 0 and 130!");
+    } else if (n >= 0 && n < 14 ) {
+        alert("Drink Toddy!");
+    } else if (n >= 14 && n < 18 ) {
+        alert("Drink Coke!");
+    } else if (n >= 18 && n < 21 ) {
+        alert("Drink Beer!");
+    } else if (n >= 21 && n < 130 ) {
+        alert("Drink Whisky!");
+    }
+}
