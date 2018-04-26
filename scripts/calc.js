09887173d4bb9f4fea3db5dd8f728b2b92d1368e
Luisa Bolado <mboladop@hotmail.com>
2018-04-26 09:53:37 +0000

    jasmine

---
+++ scripts/calc.js
@@ -0,0 +1,5 @@
+function addition(num1, num2) {
+    if (typeof(num1) == "number" && typeof(num2)  == "number" ){
+        return num1 + num2;
+    } else {alert("Arguments must be both numbers");}
+}
